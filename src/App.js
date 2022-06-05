8641777785958c7fd2ef29b08ddd62d6a0d27ef1
Shujaat Azim <shujaat.azim@gmail.com>
2022-06-04 21:13:20 -1100

    Added not found page to router list.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import LoginPage from './Pages/LoginPage';
+import NotFoundPage from './Pages/NotFoundPage';
 import ProfilePage from './Pages/ProfilePage';
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/profile" element={<ProfilePage />} />
+        <Route element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
