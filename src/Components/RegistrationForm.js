152204b12b567c61fcc6dd80c892ea34bd904d37
Shujaat Azim <shujaat.azim@gmail.com>
2022-07-04 09:27:40 -1100

    More minor changes.

--- src/Components/RegistrationForm.js
+++ src/Components/RegistrationForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const RegistrationForm = () => {
+  
   return (
     <div>
       
--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const LoginPage = () => {
+  
   return (
     <div>
       <h1>Login Page</h1>
