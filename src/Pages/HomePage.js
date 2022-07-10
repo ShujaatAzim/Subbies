8f7651f6457d7172fb827382d8c2ad04696d8fcc
Shujaat Azim <shujaat.azim@gmail.com>
2022-07-10 09:30:48 -1100

    Built out registration from scaffold.

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import RegistrationForm from '../Components/RegistrationForm';
 
 const HomePage = () => {
   
   return (
     <div>
       <h1>Home Page</h1>
+      <RegistrationForm />
     </div>
   );
 };
