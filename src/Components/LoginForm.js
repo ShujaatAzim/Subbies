fbed352442b2c36fee60985edf44a756c12ed43f
Shujaat Azim <shujaat.azim@gmail.com>
2022-07-14 22:20:25 -1100

    Added form to login.

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -5,6 +5,13 @@ const LoginForm = () => {
   return (
     <div>
       <h1>Login Form</h1>
+      <form>
+        <label>Username</label>
+        <input type="text" placeholder="username" />
+        <label>Password</label>
+        <input type="password" placeholder="password" />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
