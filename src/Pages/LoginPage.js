02373f062ef2568efbaedaa6852f94095a3f177d
Shujaat Azim <shujaat.azim@gmail.com>
2022-07-17 19:43:36 -1100

    Added login form to login page.

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React from 'react';
+import LoginForm from '../Components/LoginForm';
 
 const LoginPage = () => {
   
   return (
     <div>
       <h1>Login Page</h1>
+      <LoginForm />
     </div>
   );
 }
