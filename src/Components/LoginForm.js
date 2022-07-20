d352f1d1058f67054197bf0c13c372eff671a81d
Shujaat Azim <shujaat.azim@gmail.com>
2022-07-19 20:02:17 -1100

    Added login function.

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 const LoginForm = () => {
+
+  const login = e => {
+    e.preventDefault()
+    console.log("Logging in")
+  }
   
   return (
     <div>
       <h1>Login Form</h1>
-      <form>
+      <form onSubmit={(e) => login(e)}>
         <label>Enter Username</label>
         <input type="text" placeholder="username" />
         <label>Enter Password</label>
