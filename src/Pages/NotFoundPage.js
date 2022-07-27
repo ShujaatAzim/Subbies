3de4d72889e0341cdc6a283a11a0ef29e307f76a
Shujaat Azim <shujaat.azim@gmail.com>
2022-07-26 21:06:39 -1000

    Added functionality to home button.

--- src/Pages/NotFoundPage.js
+++ src/Pages/NotFoundPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const NotFoundPage = () => {
+
+  let navigate = useNavigate();
   
   return (
     <div>
       <h1>Page Not Found!</h1>
-      <button>Home</button>
+      <button onClick={() => navigate("/")}>Home</button>
     </div>
   );
 }
