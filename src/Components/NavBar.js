fbb9ed81ebbb128edcb6fedb5fd03971ece703a4
Shujaat Azim <shujaat.azim@gmail.com>
2022-07-28 21:22:58 -1000

    Added some functionality to nav.

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const NavBar = () => {
+
+  const navigate = useNavigate();
   
   return (
     <nav>
-      <button>Home</button>
-      <button>Properties</button>
+      <button onClick={() => navigate("/")}>Home</button>
+      <button onClick={() => navigate("/profile")}>Profile</button>
       <button>Logout</button>
     </nav>
   );
